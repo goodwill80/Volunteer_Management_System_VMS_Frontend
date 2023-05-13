e6ed42d0832147b2eea6f672d14bc68d5fb19e0e
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-05-13 09:15:52 +0800

    css

--- src/Components/Public/Home/HomeHeroImage.tsx
+++ src/Components/Public/Home/HomeHeroImage.tsx
@@ -2,7 +2,7 @@ function HomeHeroImage() {
   return (
     <div className="w-[50%] pb-8 flex flex-col justify-center items-center">
       <img
-        className="block w-[75vw] h-[400px] xl:w-[650px] xl:h-[500px]"
+        className="block w-[65vw] h-[360px] lg:w-[650px] lg:h-[500px]"
         src="https://shopee.sg/blog/wp-content/uploads/2019/11/BeFunky-collage-46.jpg"
         alt="mockup"
       />
