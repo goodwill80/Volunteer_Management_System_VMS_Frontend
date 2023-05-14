c7f706d6b8d3a196870c303e92cb34d4cb69945b
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-05-14 09:47:14 +0800

    adjust homepage

--- src/Layouts/Footer.tsx
+++ src/Layouts/Footer.tsx
@@ -3,7 +3,7 @@
 function Footer() {
   const footerYear: number = new Date().getFullYear();
   return (
-    <div className="absolute left-[-10px] bottom-[-10px] right-[-10px] w-screen xl:fixed xl:left-0 xl:right-0 lg:bottom-0 m-0 bg-blue-600 py-8 xl:w-[100%]">
+    <div className="absolute left-[-10px] bottom-[-10px] right-[-10px] w-screen xl:fixed xl:left-0 xl:right-0 xl:bottom-0 m-0 bg-blue-600 py-8 xl:w-[100%]">
       <p className="text-white text-center tracking-wider">
         Copyright &copy; {footerYear} All rights reserved
       </p>
